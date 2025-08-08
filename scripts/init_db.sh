795459786d8d6852e66d2ce6605503826f975801
Olsi Gjeci <olsi.gjeci@snyk.io>
2025-08-08 14:30:00 +0100

    upgrade script with docker flag + migrations calls

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -16,21 +16,20 @@ if ! [ -x "$(command -v sqlx)" ]; then
     exit 1
 fi
 
-# Check if a custom user has been set, otherwise default  to 'postgres'
 DB_USER="${POSTGRES_USER:=postgres}"
-# Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-# Check if a custom database has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
-# Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
-# Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
+
+# Allow to skip Docker if a dockerized Postgres database is already running 
 
 
 
 
 # Launch postgres using Docker
+if [[ -z "${SKIP_DOCKER}" ]]
+then
 docker run \
     -e POSTGRES_USER=${DB_USER} \
     -e POSTGRES_PASSWORD=${DB_PASSWORD} \
@@ -38,6 +37,7 @@ docker run \
     -p "${DB_PORT}":5432 \
     -d postgres \
     postgres -N 1000
+fi 
 
 # Keep pinging postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
@@ -47,9 +47,11 @@ until psql -h ${DB_HOST} -U ${DB_USER} -p ${DB_PORT} -d "postgres" -c '\q'; do
     sleep 1
 done
 
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
-# sqlx database create
-sqlx migrate add create_subscriptions_table
+sqlx database create
+sql migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
