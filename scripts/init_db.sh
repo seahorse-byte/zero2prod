f0beeef879e9f760870f502778ed1d8fe38c72f3
Olsi Gjeci <olsi.gjeci@snyk.io>
2025-08-08 15:58:33 +0100

    fix slq to sqlx

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -52,6 +52,6 @@ done
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
-sql migrate run
+sqlx migrate run
 
 >&2 echo "Postgres has been migrated, ready to go!"
