66ee7b5f4ee4729f68eb6e8395dfce1fae361609
Olsi Gjeci <olsi.gjeci@snyk.io>
2025-08-08 08:12:09 +0100

    create subscription table sql

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -51,4 +51,5 @@ done
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
-sqlx database create
+# sqlx database create
+sqlx migrate add create_subscriptions_table
