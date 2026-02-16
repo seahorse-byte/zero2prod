76f3b49dbe5abef0edf83de521fb460e048418b1
Olsi Gjeci <olsigjeci@proton.me>
2026-02-16 09:22:32 +0000

    with tracing_log

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,26 @@ use zero2prod::startup::run;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+
+    // Initialize the logger
+    LogTracer::init().expect("Failed to set logger");
+
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new("zero2prod.log".into(), std::io::stdout);
 
-
-let subscriber = Registry::default()
-    .with(env_filter)
-    .with(JsonStorageLayer)
-    .with(formatting_layer);
+    // Create the subscriber
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
 
     set_global_default(subscriber).expect("Failed to set subscriber");
+
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
@@ -27,5 +33,6 @@ let subscriber = Registry::default()
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())    // Return Ok to indicate successful execution
 }
